5f864fc1d0d8c0f7355019b36cb5e4c15b97ba01
theusdorne <matheus.dorne@gmail.com>
2023-06-23 18:45:44 -0300

    Update pagamento.php

--- pagamento.php
+++ pagamento.php
@@ -36,20 +36,54 @@
             <div id="blackfriday">BLACK FRIDAY COM ATÉ 70% OFF</div>  
   </header>
 
-  <main>
+  <main> 
+    <div class="detalhe-entrega"> 
+
+    </div>
     <div class="tela-pagamento">
       <h1>Detalhes do Pagamento</h1>
       <form>
         <ul class="detalhes-pagamento">
           <li>
-            <label for="nome">Nome no Cartão:</label>
+            <label >Nome no Cartão:</label>
             <input type="text" id="nome" placeholder="Insira o nome no cartão" required>
           </li>
           <li>
-            <label for="numero-cartao">Número do Cartão:</label>
+            <label >Número do Cartão:</label>
             <input type="text" id="numero-cartao" placeholder="Insira o número do cartão" required>
+          </li>  
+          <li>
+            <label >Validade:</label>
+            <input type="date" id="valide-cartao"  required>
           </li>   
-          <!-- Outros campos de pagamento -->
+          <li> 
+            <label >Parcelas:</label>
+            <select id="parcelas" required>
+              <option value="1">1x de R$ 150,00</option>
+              <option value="2">2x de R$ 75,00</option>
+              <option value="3">3x de R$ 50,00</option>
+              <option value="4">4x de R$ 37,50</option>
+              <option value="5">5x de R$ 30,00</option>
+              <option value="6">6x de R$ 25,00</option>
+              <option value="7">7x de R$ 21,43</option>
+              <option value="8">8x de R$ 18,75</option>
+              <option value="9">9x de R$ 16,67</option>
+              <option value="10">10x de R$ 15,00</option>
+              <option value="11">11x de R$ 13,64</option>
+              <option value="12">12x de R$ 12,50</option> 
+            </li>
+            <li>
+                <label >CVV:</label>
+                <input type="text" id="cvv-cartao" placeholder="Insira o CVV" required>  
+            </li>
+            <li>
+                <label >CPF:</label>
+                <input type="text" id="cpf-cartao" placeholder="Insira o CPF" required>
+            </li>
+            <li>
+                <input type="button" id="btn-pagar" value="Pagar">
+            </li>
+         
         </ul>
         <button class="btn-pagar">Pagar</button>
       </form>
