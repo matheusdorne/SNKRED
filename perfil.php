4f43b65c1db96c879a548140b4678dda3b71d4a6
theusdorne <matheus.dorne@gmail.com>
2023-06-23 19:06:27 -0300

    ajustes

--- perfil.php
+++ perfil.php
@@ -1,73 +1,142 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Perfil do Cliente</title>
-  <link rel="stylesheet" href="styles.css"> 
-  <link rel="stylesheet" href="perfil.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perfil do Cliente</title>
+    <link rel="stylesheet" href="header.css">
+    <link rel="stylesheet" href="perfil.css">
 </head>
+
 <body>
-  <header>
-    <!-- Conteúdo do cabeçalho -->
-  </header>
+   
+        <header>
+            <div id="div_head">
+                <ul id="header_snkred">
+                    <li>
+                        <a href="main.html">
+                            <img class="logo-snkred" src="imagens/logo_snkred.png" alt="LOGO DO SITE SNKRED">
+                        </a>
+                    </li>
+                    <li>
+                        <div class="separador"></div>
+                    </li>
+                    <li>
+                        <form action="login.html">
+                            <button type="submit" class="loginBtn">LOGIN</button>
+                        </form>
+                    </li>
+                    <li>
+                        <a href="carrinho.html">
+                            <img class="cartbtn" src="imagens/logo_shoppingcart.png" alt="ICONE DE CARRINHO DE COMPRAS">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </header>
+    
 
-  <main>
-    <div class="perfil-cliente">
-      <h1>Perfil do Cliente</h1>
-      <div class="dados-pessoais">
-        <h3>Dados Pessoais</h3>
-        <ul>
-          <li>
-            <span class="label">Nome:</span>
-            <span class="valor">John Doe</span>
-          </li>
-          <li>
-            <span class="label">E-mail:</span>
-            <span class="valor">johndoe@example.com</span>
-          </li>
-          <li>
-            <span class="label">Telefone:</span>
-            <span class="valor">123-456-7890</span>
-          </li>
-          <!-- Outros dados pessoais -->
-        </ul>
-      </div>
+    <main>
+        <div class="perfil-cliente">
+            <h1>Perfil do Cliente</h1>
+            <div class="dados-pessoais">
+                <h3>Dados Pessoais</h3>
+                <ul>
+                    <li>
+                        <span class="label">Nome:</span>
+                        <span class="valor">John Doe</span>
+                    </li>
+                    <li>
+                        <span class="label">E-mail:</span>
+                        <span class="valor">johndoe@example.com</span>
+                    </li>
+                    <li>
+                        <span class="label">Telefone:</span>
+                        <span class="valor">123-456-7890</span>
+                    </li>
+                    <!-- Outros dados pessoais -->
+                </ul>
+            </div>
 
-      <div class="enderecos">
-        <h3>Endereços</h3>
-        <ul>
-          <li>
-            <span class="label">Endereço de Cobrança:</span>
-            <span class="valor">Rua Principal, 123</span>
-          </li>
-          <li>
-            <span class="label">Endereço de Entrega:</span>
-            <span class="valor">Avenida Secundária, 456</span>
-          </li>
-          <!-- Outros endereços -->
-        </ul>
-      </div>
+            <div class="enderecos">
+                <h3>Endereços</h3>
+                <ul>
+                    <li>
+                        <span class="label">Endereço de Cobrança:</span>
+                        <span class="valor">Rua Principal, 123</span>
+                    </li>
+                    <li>
+                        <span class="label">Endereço de Entrega:</span>
+                        <span class="valor">Avenida Secundária, 456</span>
+                    </li>
+                    <!-- Outros endereços -->
+                </ul>
+            </div>
 
-      <div class="pedidos">
-        <h3>Pedidos Recentes</h3>
-        <ul>
-          <li>
-            <span class="label">Número do Pedido:</span>
-            <span class="valor">12345</span>
-          </li>
-          <li>
-            <span class="label">Data do Pedido:</span>
-            <span class="valor">10 de março de 2023</span>
-          </li>
-          <!-- Outros pedidos recentes -->
-        </ul>
-      </div>
-    </div>
-  </main>
 
-  <footer>
-    <!-- Conteúdo do rodapé -->
-  </footer>
+        </div>
+    </main>
+
+  
+        <footer>
+            <div class="footer">
+                <nav class="footer">
+                    <ul>
+                        <li>
+                            <h3>AJUDA E SUPORTE</h3>
+                        </li>
+                        <li>
+                            <a href="">Sobre nós</a>
+                        </li>
+                        <li>
+                            <a href="">Contato</a>
+                        </li>
+                        <li>
+                            <a href="">Formas de Pagamento</a>
+                        </li>
+                    </ul>
+                </nav>
+                <nav class="footer">
+                    <ul>
+                        <li>
+                            <h3>CATEGORIAS</h3>
+                        </li>
+                        <li>
+                            <a href="">Skate</a>
+                        </li>
+                        <li>
+                            <a href="">Casual</a>
+                        </li>
+                        <li>
+                            <a href="">Basquete</a>
+                        </li>
+                    </ul>
+                </nav>
+                <nav class="footer">
+                    <ul class="socialBtn">
+                        <li>
+                            <h3>REDE SOCIAIS</h3>
+                        </li>
+                        <li>
+                            <a href="https://pt-br.facebook.com/">
+                                <img src="imagens/fbicon.png" alt="ICONE DO FACEBOOK">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.instagram.com/">
+                                <img src="imagens/insticon.png" alt="ICONE DO INSTAGRAM">
+                            </a>
+                        </li>
+                        <li> <a href="https://twitter.com/login?lang=pt">
+                                <img src="imagens/twiticon.png" alt="ICONE DO TWITTER">
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </footer>
+  
 </body>
+
 </html>
--- ticket.php
+++ ticket.php
@@ -1,61 +1,126 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Enviar Ticket de Suporte</title>
-  <link rel="stylesheet" href="styles.css"> 
-  <link rel="stylesheet" href="ticket.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Enviar Ticket de Suporte</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="ticket.css">
 </head>
+
 <body>
-  <header>
-    <nav>
-      <ul id="header_snkred">
-        <li><a href="index.html"><img src="logo.png" alt="Logo" class="logo-snkred"></a></li>
-        <li class="loginBtnLi"><a href="login.html" class="loginBtn">Login</a></li>
-        <li><a href="carrinho.html"><img src="cart.png" alt="Carrinho" class="cartbtn"></a></li>
-      </ul>
-    </nav>
-  </header>
 
-  <main>
-    <div class="enviar-ticket">
-      <h1>Enviar Ticket de Suporte</h1>
-      <form action="enviar_ticket.php" method="POST">
-        <div class="campo">
-          <label for="nome">Nome:</label>
-          <input type="text" id="nome" name="nome" required>
+    <header>
+        <div id="div_head">
+            <ul id="header_snkred">
+                <li>
+                    <a href="main.html">
+                        <img class="logo-snkred" src="imagens/logo_snkred.png" alt="LOGO DO SITE SNKRED">
+                    </a>
+                </li>
+                <li>
+                    <div class="separador"></div>
+                </li>
+                <li>
+                    <form action="login.html">
+                        <button type="submit" class="loginBtn">LOGIN</button>
+                    </form>
+                </li>
+                <li>
+                    <a href="carrinho.html">
+                        <img class="cartbtn" src="imagens/logo_shoppingcart.png" alt="ICONE DE CARRINHO DE COMPRAS">
+                    </a>
+                </li>
+            </ul>
         </div>
-        <div class="campo">
-          <label for="email">E-mail:</label>
-          <input type="email" id="email" name="email" required>
-        </div>
-        <div class="campo">
-          <label for="assunto">Assunto:</label>
-          <input type="text" id="assunto" name="assunto" required>
-        </div>
-        <div class="campo">
-          <label for="mensagem">Mensagem:</label>
-          <textarea id="mensagem" name="mensagem" rows="5" required></textarea>
-        </div>
-        <button type="submit" class="enviar-btn">Enviar</button>
-      </form>
-    </div>
-  </main>
+    </header>
 
-  <footer>
-    <div class="footer">
-      <ul>
-        <li><a href="#">Sobre Nós</a></li>
-        <li><a href="#">Contato</a></li>
-        <li><a href="#">Política de Privacidade</a></li>
-      </ul>
-      <ul class="socialBtn">
-        <li><a href="#"><img src="facebook.png" alt="Facebook"></a></li>
-        <li><a href="#"><img src="instagram.png" alt="Instagram"></a></li>
-        <li><a href="#"><img src="twitter.png" alt="Twitter"></a></li>
-      </ul>
-    </div>
-  </footer>
+
+    <main>
+        <div class="enviar-ticket">
+            <h1>Enviar Ticket de Suporte</h1>
+            <form action="enviar_ticket.php" method="POST">
+                <div class="campo">
+                    <label for="nome">Nome:</label>
+                    <input type="text" id="nome" name="nome" required>
+                </div>
+                <div class="campo">
+                    <label for="email">E-mail:</label>
+                    <input type="email" id="email" name="email" required>
+                </div>
+                <div class="campo">
+                    <label for="assunto">Assunto:</label>
+                    <input type="text" id="assunto" name="assunto" required>
+                </div>
+                <div class="campo">
+                    <label for="mensagem">Mensagem:</label>
+                    <textarea id="mensagem" name="mensagem" rows="5" required></textarea>
+                </div>
+                <button type="submit" class="enviar-btn">Enviar</button>
+            </form>
+        </div>
+    </main>
+
+    <footer>
+        <footer>
+            <div class="footer">
+                <nav class="footer">
+                    <ul>
+                        <li>
+                            <h3>AJUDA E SUPORTE</h3>
+                        </li>
+                        <li>
+                            <a href="">Sobre nós</a>
+                        </li>
+                        <li>
+                            <a href="">Contato</a>
+                        </li>
+                        <li>
+                            <a href="">Formas de Pagamento</a>
+                        </li>
+                    </ul>
+                </nav>
+                <nav class="footer">
+                    <ul>
+                        <li>
+                            <h3>CATEGORIAS</h3>
+                        </li>
+                        <li>
+                            <a href="">Skate</a>
+                        </li>
+                        <li>
+                            <a href="">Casual</a>
+                        </li>
+                        <li>
+                            <a href="">Basquete</a>
+                        </li>
+                    </ul>
+                </nav>
+                <nav class="footer">
+                    <ul class="socialBtn">
+                        <li>
+                            <h3>REDE SOCIAIS</h3>
+                        </li>
+                        <li>
+                            <a href="https://pt-br.facebook.com/">
+                                <img src="imagens/fbicon.png" alt="ICONE DO FACEBOOK">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.instagram.com/">
+                                <img src="imagens/insticon.png" alt="ICONE DO INSTAGRAM">
+                            </a>
+                        </li>
+                        <li> <a href="https://twitter.com/login?lang=pt">
+                                <img src="imagens/twiticon.png" alt="ICONE DO TWITTER">
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </footer>
+    </footer>
 </body>
+
 </html>
